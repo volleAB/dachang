baa44eab94ca8fc2c5b4f3110ea656e02098349f
volleAB <724958674@qq.com>
2018-07-02 18:10:45 +0800

    todo

--- crawler.js
+++ crawler.js
@@ -3,13 +3,23 @@ const cheerio = require('cheerio');
 var promise = require('bluebird');
 const url = ['','https://www.scuec.edu.cn/s/329/t/1619/p/2/list.htm'];
 const fs = require('fs');
-var page = [];
-var PageNum = 0;
+// var page = [];
+// var pageNum = 0;
+var pageBrief = [];
 
 crawler = () => {
-    getPageList = (url) => {
-        // var page = [];
-        // var pages = [];
+    
+    getPageList = (url, numj) => {
+        var page = [];
+        var page0 = {
+            list: []
+        }
+        var page1 = {
+            list: []
+        }
+        var page2 = {
+            list: []
+        }
         http.get(url, (res) => {
             var html = '';
             res.on('data', (data) => {
@@ -36,13 +46,25 @@ crawler = () => {
                         titleTime: titleTime
                     }
                     // console.log(titleList);
-                    page.push(titleList);
+                    if (!numj) {
+                        page0.list.push(titleList);
+                        page.push(page0);
+                    }
+                    else if (numj == 1) {
+                        page1.list.push(titleList);
+                        page.push(page1);
+                    }
+                    else if (numj == 2) {
+                        page2.list.push(titleList);
+                        page.push(page2);
+                    }
+                    else {
+                        console.log('over');
+                    }
 
                     //获取文章摘要
                     //todo
-                    
 
-                    //获取后三页的信息
                     /**
                      * http.get(titleList.titleHref, (res) => {
                         var html = '';
@@ -67,21 +89,21 @@ crawler = () => {
                     })
                      */
                     
-
-                    /*
                     getMes(titleList.titleHref)
                         .then((res) => {
-                            
-                            titleList["titleBrief"] = res;
-                            page.push(titleList);
-                            console.log('one');
-                            var buf = new Buffer(pages);
-                            var buf = new Buffer(page); //存放二进制数据的缓存区
-                            fs.writeFile('./message.json', JSON.stringify(page), function(err) {
+                            var brief = {
+                                titleHref: titleList.titleHref,
+                                titleTime: titleList.titleTime,
+                                titleBrief: res
+                            }
+                            // titleList["titleBrief"] = res;
+                            pageBrief.push(brief);
+                            var buf = new Buffer(pageBrief); //存放二进制数据的缓存区
+                            fs.writeFile('./brief.json', JSON.stringify(pageBrief), function(err) {
                                 if (err) console.log('写文件操作失败');
                             });
                         })
-                        */
+
                     var buf = new Buffer(page); //存放二进制数据的缓存区
                     fs.writeFile('./message.json', JSON.stringify(page), function(err) {
                         if (err) console.log('写文件操作失败');
@@ -90,18 +112,21 @@ crawler = () => {
             })
         })
     }
-    getNextPageList = (num) => {
-        // console.log(num);
-        // https://www.scuec.edu.cn/s/329/t/1619/p/2/i/65/list.htm
-        for(var i = num-1; i > 62; i--) {
+
+    //获取后三页的信息
+
+    /**
+     * getNextPageList = (num) => {
+        for(var i = num-1, j = 1; i > 63; i--, j++) {
             var url = 'https://www.scuec.edu.cn/s/329/t/1619/p/2/i/'+i+'/list.htm';
-            // console.log(url);
-            getPageList(url);
-            // console.log(i);
+            getPageList(url, j);
         }
     }
+     */
+    
+
     getPageList(url[1]);
-    getNextPageList(66);
+    // getNextPageList(66);
 }
 
 //获取文章摘要
@@ -117,7 +142,7 @@ getMes = (url) => {
                 // console.log(titleList)
                 var $ = cheerio.load(html);
                 var mes = $('.single-content').text();
-                var Brief = mes.slice(10,100);
+                var Brief = mes.replace(/(^\s*)|(\s*$)/g, "").slice(0,100)
                 // titleList["titleBrief"] = Brief;
                 // console.log(Brief);
                 reslove(Brief);
